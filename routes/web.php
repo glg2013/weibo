c3aff1f4ab10fcfa2afaef6363f89de3ee6f98b7
glg2013 <57412960@qq.com>
2025-01-02 09:17:52 +0000

    基础页面

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\StaticPagesController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
+Route::get('/', [StaticPagesController::class, 'home']);
+
+Route::get('/help', [StaticPagesController::class, 'help']);
+
+Route::get('/about', [StaticPagesController::class, 'about']);
